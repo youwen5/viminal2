77ed4bd95039beb8b5085c695f6a2d924187bc42
Youwen Wu <youwenw@gmail.com>
2024-10-11 23:53:54 -0700

    fix: better blink-cmp config, no more render-markdown

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -4,20 +4,6 @@ return {
     after = function()
       require("render-markdown").setup({
         file_types = { "markdown" },
-        overrides = {
-          buftype = {
-            nofile = {
-              render_modes = { "n", "c", "i" },
-              debounce = 5,
-              code = {
-                left_pad = 0,
-                right_pad = 0,
-                language_pad = 0,
-              },
-            },
-          },
-          filetype = {},
-        },
       })
     end,
   },
