0f1039ca8f0a0d848360a421aed84c3e15d6f131
Youwen Wu <youwenw@gmail.com>
2024-12-28 00:57:14 -0800

    feat: new and improved blink.cmp config

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,13 +1,30 @@
 return {
-  "blink-cmp",
+  "blink.cmp",
   event = "BufEnter",
   after = function()
-    require("blink-cmp").setup({
-      -- for keymap, all values may be string | string[]
-      -- use an empty table to disable a keymap
-      keymap = {
-        preset = "default",
+    require("blink.cmp").setup({
+      keymap = { preset = "default" },
+
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "normal",
       },
+
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+      completion = {
+        menu = { border = "single" },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+          update_delay_ms = 50,
+          treesitter_highlighting = true,
+          window = { border = "single" },
+        },
+      },
+      signature = { window = { border = "single" } },
+      fuzzy = { prebuilt_binaries = { force_version = "" } },
     })
   end,
 }
