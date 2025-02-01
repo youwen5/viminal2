c34683821c925dce55cfb6d1ccc8a8059bddf662
Youwen Wu <youwenw@gmail.com>
2025-02-01 14:50:32 -0800

    feat: fennelize latex

--- lua/plugins/by-lang/latex.lua
+++ lua/plugins/by-lang/latex.lua
@@ -1,8 +1,6 @@
-return {
-  "texpresso.vim",
-  filetypes = { "*.tex" },
-  cmd = { "TeXpresso" },
-  after = function()
-    require("texpresso").texpresso_path = nixCats("bin.texpresso")
-  end,
-}
+-- [nfnl] Compiled from ./fnl/plugins/by-lang/latex.fnl by https://github.com/Olical/nfnl, do not edit.
+local function _1_()
+  require("texpresso")["texpresso_path"] = nixCats("bin.texpresso")
+  return nil
+end
+return {"texpresso.vim", after = _1_, cmd = {"TeXpresso"}, filetypes = {"*.tex"}}
