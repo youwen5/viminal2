0fc9dfe029b14b64706412b52881d70efefccac6
Youwen Wu <youwenw@gmail.com>
2025-02-01 14:51:07 -0800

    feat: fennelize toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,42 +1,17 @@
-return {
-  "toggleterm.nvim",
-  keys = {
-    {
-      "<C-l>",
-      function()
-        vim.cmd("ToggleTerm direction=float")
-      end,
-      mode = { "n", "t", "v" },
-      desc = "Toggle terminal",
-    },
-    {
-      "<leader>tv",
-      function()
-        vim.cmd("ToggleTerm direction=vertical")
-      end,
-      desc = "Vertical terminal",
-      mode = "n",
-    },
-    {
-      "<leader>tt",
-      function()
-        vim.cmd("ToggleTerm direction=horizontal")
-      end,
-      desc = "Horizontal terminal",
-      mode = "n",
-    },
-    {
-      "<leader>ts",
-      function()
-        vim.cmd("TermSelect")
-      end,
-      desc = "Terminal select",
-      mode = "n",
-    },
-  },
-  after = function()
-    require("toggleterm").setup({
-      shade_terminals = false,
-    })
-  end,
-}
+-- [nfnl] Compiled from ./fnl/plugins/toggleterm.fnl by https://github.com/Olical/nfnl, do not edit.
+local function _1_()
+  return require("toggleterm").setup({shade_terminals = false})
+end
+local function _2_()
+  return vim.cmd("ToggleTerm direction=float")
+end
+local function _3_()
+  return vim.cmd("ToggleTerm direction=vertical")
+end
+local function _4_()
+  return vim.cmd("ToggleTerm direction=horizontal")
+end
+local function _5_()
+  return vim.cmd("TermSelect")
+end
+return {"toggleterm.nvim", after = _1_, keys = {{"<C-l>", _2_, desc = "Toggle terminal", mode = {"n", "t", "v"}}, {"<leader>tv", _3_, desc = "Vertical terminal", mode = "n"}, {"<leader>tt", _4_, desc = "Horizontal terminal", mode = "n"}, {"<leader>ts", _5_, desc = "Terminal select", mode = "n"}}}
