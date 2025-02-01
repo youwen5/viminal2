06e3caa65ec06fa58e27f767629f5a552d4b5a6a
Youwen Wu <youwenw@gmail.com>
2025-02-01 14:22:35 -0800

    feat: fennelize obsidian-sync

--- lua/scripts/obsidian-sync.lua
+++ lua/scripts/obsidian-sync.lua
@@ -1,14 +1,12 @@
-vim.api.nvim_create_user_command("VaultSync", function()
+-- [nfnl] Compiled from ./fnl/scripts/obsidian-sync.fnl by https://github.com/Olical/nfnl, do not edit.
+local function _1_()
   local date = os.date("%Y-%m-%d %H:%M:%S")
-  local commit_commands = {
-    "git add -A",
-    string.format([[git commit -m "auto-update(nvim): %s"]], date),
-  }
+  local commit_commands = {"git add -A", string.format("git commit -m \"auto-update(nvim): %s\"", date)}
   for _, cmd in ipairs(commit_commands) do
     vim.fn.system(cmd)
   end
   vim.notify("Committed all local changes.", vim.log.levels.INFO)
-  vim.cmd("Neogit push")
-end, {})
-
-vim.keymap.set("n", "<leader>os", vim.cmd.VaultSync)
+  return vim.cmd("Neogit push")
+end
+vim.api.nvim_create_user_command("VaultSync", _1_, {})
+return vim.keymap.set("n", "<leader>os", vim.cmd.VaultSync)
