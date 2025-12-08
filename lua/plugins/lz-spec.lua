1f1b57f7ffa91dc6b816d467c3f0142b35f2fb52
Youwen Wu <youwenw@gmail.com>
2025-12-07 19:27:06 -0800

    feat: transparent background of dropbar.nvim

--- lua/plugins/lz-spec.lua
+++ lua/plugins/lz-spec.lua
@@ -280,6 +280,10 @@ return {
       vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
       vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
       vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+
+      -- make winbar, including dropbar.nvim transparent <https://github.com/folke/tokyonight.nvim/issues/703>
+      vim.api.nvim_set_hl(0, "WinBar", { bg = "NONE" })
+      vim.api.nvim_set_hl(0, "WinBarNC", { bg = "NONE" })
     end,
   },
   {
